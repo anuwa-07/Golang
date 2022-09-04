f8f84dbfb5dd96b0eb2d395272f9d61147558fb6
Anuruddha <anuruddha@prodoscore.com>
2022-09-04 17:58:25 +0530

    chore: update middleware section

--- net-http-package/middlewares/middlewares.go
+++ net-http-package/middlewares/middlewares.go
@@ -42,6 +42,29 @@ func loggingHandler(next http.Handler) http.Handler {
 	})
 }
 
+// Http Middlerware accept and return a http.Handler type func. So due to this, we can call combine multiple middlewares to some job done.
+func middlewareSecond(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("MiddlewareSecond - Before Handler")
+
+		if r.URL.Path == "/message" {
+			if r.URL.Query().Get("password") == "password@123" {
+                log.Println("Authorized to the system")
+                next.ServeHTTP(w, r)				
+			} else {
+                log.Println("Failed to authorize to the system")
+                return				
+			}
+		}else {
+			next.ServeHTTP(w, r)
+		}
+
+		log.Println("MiddlewareSecond - After Handler")
+	})
+}
+
+
+
 func index(w http.ResponseWriter, r *http.Request) {
 	log.Println("Executing index handler")
 	fmt.Fprintf(w, "Welcome!")
@@ -52,6 +75,12 @@ func about(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Go Middleware")
 }
 
+func message(w http.ResponseWriter, r *http.Request) {
+    log.Println("Executing message Handler")
+    fmt.Fprintf(w, "HTTP Middleware is awesome")
+}
+
+
 func iconHandler(w http.ResponseWriter, r *http.Request) {
 }
 
@@ -60,8 +89,12 @@ func main() {
 	http.HandleFunc("/favicon.ico", iconHandler)
 	indexHandler := http.HandlerFunc(index)
 	aboutHandler := http.HandlerFunc(about)
+	messageHandler := http.HandlerFunc(message)
+
 	http.Handle("/", loggingHandler(indexHandler))
 	http.Handle("/about", loggingHandler(aboutHandler))
+	http.Handle("/message", middlewareSecond(loggingHandler(messageHandler))) // multiple middleware usecases
+
 
 	server := &http.Server{
 		Addr: ":8080",
